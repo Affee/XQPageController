2a320fb3cff75ed9118d9bcda7077d2701e28cd6
EthanD <m13120158558@163.com>
2018-11-12 10:39:01 +0800

    删除删除

--- Podfile
+++
@@ -1,5 +0,0 @@
-target:’XQPageControllerDemo’ do
-platform :ios,'8.0'
-pod 'Masonry'
-pod 'YYCategories'
-end
--- Pods/Target Support Files/Pods-XQPageControllerDemo/Pods-XQPageControllerDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XQPageControllerDemo : NSObject
-@end
-@implementation PodsDummy_Pods_XQPageControllerDemo
-@end
